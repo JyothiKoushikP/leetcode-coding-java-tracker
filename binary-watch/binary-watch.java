67292cad44010a43ad811e89ce1c6fc243fa3389
Jyothi Koushik Paladugu <47804397+jyothi-koushik-1998@users.noreply.github.com>
2023-06-05 22:49:12 -0400

    Time: 7 ms (67.24%) | Memory: 41.3 MB (87.30%) - LeetSync

--- binary-watch/binary-watch.java
+++ binary-watch/binary-watch.java
@@ -1,27 +1,29 @@
 class Solution {
-    public List<String> readBinaryWatch(int num) {
-        List<String> result = new ArrayList<String>();
-        
-        for (int i = 0; i < 12; i++) {
-            for (int j = 0; j < 60; j++) {
-                if (hDist(i) + hDist(j) == num) {
-                    result.add(String.format("%d:%02d", i, j));
+    public List<String> readBinaryWatch(int turnedOn) {
+        int[] hours = new int[12];
+        int[] minutes = new int[60];
+        countOnebits(hours);
+        countOnebits(minutes);
+        List<String> result = new ArrayList();
+        for(int i = 0; i < hours.length; i++) {
+            for(int j = 0; j < minutes.length; j++) {
+                if(hours[i] + minutes[j] == turnedOn) {
+                    String res = "";
+                    if((Integer.toString(j)).length() == 1) {
+                        res = Integer.toString(i) + ":0" + Integer.toString(j);
+                    } else {
+                        res = Integer.toString(i) + ":" + Integer.toString(j);
+                    }
+                        result.add(res);
                 }
             }
         }
-        
         return result;
     }
-    
-	// Count bits with Hamming Distance
-    public int hDist(int num) {
-        int ans = 0;
-        
-        while (num != 0) {
-            ans++;
-            num = num & (num - 1);
+
+    public void countOnebits(int[] arr) {
+        for(int i = 1; i < arr.length;i++) {
+            arr[i] = arr[i & (i-1)] + 1;
         }
-        
-        return ans;
     }
 }
