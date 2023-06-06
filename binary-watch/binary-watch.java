b1ef34629f01a319d7de50ff0d9addebca0961a1
Jyothi Koushik Paladugu <47804397+jyothi-koushik-1998@users.noreply.github.com>
2023-06-05 22:50:00 -0400

    Time: 7 ms (67.24%) | Memory: 41.4 MB (82.60%) - LeetSync

--- binary-watch/binary-watch.java
+++ binary-watch/binary-watch.java
@@ -4,19 +4,27 @@ class Solution {
         int[] minutes = new int[60];
         countOnebits(hours);
         countOnebits(minutes);
+        int h = turnedOn;
+        int m = 0;
         List<String> result = new ArrayList();
-        for(int i = 0; i < hours.length; i++) {
-            for(int j = 0; j < minutes.length; j++) {
-                if(hours[i] + minutes[j] == turnedOn) {
-                    String res = "";
-                    if((Integer.toString(j)).length() == 1) {
-                        res = Integer.toString(i) + ":0" + Integer.toString(j);
-                    } else {
-                        res = Integer.toString(i) + ":" + Integer.toString(j);
+        while(h >= 0 && m <= turnedOn) {
+            for(int i = 0; i < hours.length; i++) {
+                if(hours[i] == h) {
+                    for(int j = 0; j < minutes.length; j++) {
+                        if(minutes[j] == m) {
+                            String res = "";
+                            if((Integer.toString(j)).length() == 1) {
+                                res = Integer.toString(i) + ":0" + Integer.toString(j);
+                            } else {
+                                res = Integer.toString(i) + ":" + Integer.toString(j);
+                            }
+                            result.add(res);
+                        }
                     }
-                        result.add(res);
                 }
             }
+            h--;
+            m++;
         }
         return result;
     }
