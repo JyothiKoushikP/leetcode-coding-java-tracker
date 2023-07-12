527ba889f32104d63e66aad35d04c0276d3b6793
Jyothi Koushik Paladugu <47804397+jyothi-koushik-1998@users.noreply.github.com>
2023-07-11 20:32:44 -0700

    Time: 18 ms (18.41%) | Memory: 46.3 MB (5.43%) - LeetSync

--- k-diff-pairs-in-an-array/k-diff-pairs-in-an-array.java
+++ k-diff-pairs-in-an-array/k-diff-pairs-in-an-array.java
@@ -1,25 +1,19 @@
 class Solution {
     public int findPairs(int[] nums, int k) {
-        int n = nums.length, count = 0;
-        StringBuilder res;
-        ArrayList<Integer> num = new ArrayList();
-        HashSet<String> pairs = new HashSet();
-        for(int i = 0;i < n;i++) {
-            res = new StringBuilder();
-            int ele = nums[i];
-            if(num.contains(ele + k)) {
-                res.append(Integer.toString(ele));
-                res.append(Integer.toString(ele + k));
-                pairs.add(res.toString());
-            } 
-            res = new StringBuilder();
-            if(num.contains(ele - k)) {
-                res.append(Integer.toString(ele - k));
-                res.append(Integer.toString(ele));
-                pairs.add(res.toString());
+        HashSet<String> pairs = new HashSet<>();
+        HashSet<Integer> numSet = new HashSet<>();
+
+        for (int ele : nums) {
+            if (numSet.contains(ele + k)) {
+                pairs.add((ele + k) + "-" + ele);
             }
-            num.add(ele);
+            if (numSet.contains(ele - k)) {
+                pairs.add(ele + "-" + (ele - k));
+            }
+            numSet.add(ele);
         }
+
         return pairs.size();
-    }
+}
+
 }
