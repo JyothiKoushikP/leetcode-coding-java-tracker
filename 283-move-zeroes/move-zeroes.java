3c631cd792706cc4e6a991acd9672be8a61aa8ff
Jyothi Koushik Paladugu <47804397+jyothi-koushik-1998@users.noreply.github.com>
2023-09-16 08:15:44 -0700

    Time: 3 ms (23.11%) | Memory: 45.2 MB (43.29%) - LeetSync

--- 283-move-zeroes/move-zeroes.java
+++ 283-move-zeroes/move-zeroes.java
@@ -1,14 +1,23 @@
 class Solution {
     public void moveZeroes(int[] nums) {
-        int i = 0,j = 0;
-        while(j < nums.length) {
-            if(nums[j] != 0){
-                int temp = nums[i];
-                nums[i] = nums[j];
-                nums[j] = temp;
-                i++;
-            }
-            j++;
-        }
+       int zp = Integer.MIN_VALUE, np = Integer.MIN_VALUE;
+       for(int i = 0; i < nums.length; i++) {
+           if(nums[i] == 0) {
+               if(zp == Integer.MIN_VALUE) {
+                   zp = i;
+               }
+           } else {
+               if(zp != Integer.MIN_VALUE) {
+                   swap(nums,zp,i);
+                   zp++;
+               }
+           }
+       }
+    }
+
+    public void swap(int[] nums, int pos1, int pos2) {
+        int temp = nums[pos1];
+        nums[pos1] = nums[pos2];
+        nums[pos2] = temp;
     }
 }
