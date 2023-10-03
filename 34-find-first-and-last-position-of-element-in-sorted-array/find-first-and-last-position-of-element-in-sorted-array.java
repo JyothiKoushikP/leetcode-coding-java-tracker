e937118818c121dd852ba740812ecc4ee9081e19
Jyothi Koushik Paladugu <47804397+jyothi-koushik-1998@users.noreply.github.com>
2023-10-03 09:04:22 -0700

    Time: 0 ms (100.00%) | Memory: 44.2 MB (71.46%) - LeetSync

---
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
@@ -0,0 +1,50 @@
+class Solution {
+    public int[] searchRange(int[] nums, int target) {
+        boolean isFirstIndex = true;
+        int n = nums.length;
+        int firstOccurence = binarySearch(nums,target,true,0,n-1);
+        if(firstOccurence == -1) {
+            return new int[] {-1, -1};
+        }
+        int secondOccurence = binarySearch(nums,target,false,0,n-1);
+        if(secondOccurence == -1) {
+            return new int[] {firstOccurence,firstOccurence};
+        }
+        return new int[] {firstOccurence, secondOccurence};
+    }
+
+    public int binarySearch(int[] arr, int target, boolean isFirstIndex,int start,int end) {
+        int n = arr.length;
+        while(start <= end) {
+            int mid = start + (end-start)/2;
+            if(arr[mid] == target) {
+                if(isFirstIndex) {
+                    if(mid > 0) {
+                        if(arr[mid - 1] == target) {
+                            end = mid - 1;
+                        } else {
+                            return mid;
+                        }
+                    } else {
+                        return mid;
+                    }
+                } else {
+                    if(mid < n-1 ){
+                        if(arr[mid + 1] == target) {
+                            start = mid + 1;
+                        } else {
+                            return mid;
+                        }
+                    } else {
+                        return mid;
+                    }
+                } 
+            } else if (arr[mid] < target) {
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+            }
+        }
+        return -1;
+    }
+}
