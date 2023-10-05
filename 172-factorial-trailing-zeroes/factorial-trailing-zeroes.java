7b9ca64db715b51641e90c7053abdb5c0bb1b8e3
Jyothi Koushik Paladugu <47804397+jyothi-koushik-1998@users.noreply.github.com>
2023-10-05 16:32:52 -0700

    Time: 1 ms (16.23%) | Memory: 39.5 MB (39.11%) - LeetSync

--- 172-factorial-trailing-zeroes/factorial-trailing-zeroes.java
+++ 172-factorial-trailing-zeroes/factorial-trailing-zeroes.java
@@ -1,30 +1,12 @@
 class Solution {
     public int trailingZeroes(int n) {
-        if(n == 0) {
-            return 0;
-        }
-        int twoCount = 0, fiveCount = 0;
-        for(int i = 1; i <= n; i++) {
-            if(i % 2 == 0){
-                twoCount += getFactorCount(i,2);
-            }
-            if(i % 5 == 0){
-                fiveCount += getFactorCount(i,5);
-            }
+        int powerOf5 = 5;
+        int res = 0;
+        while(n >= powerOf5) {
+            res += (n/powerOf5);
+            powerOf5 *= 5;
         }
 
-        if(twoCount == 0 || fiveCount == 0) {
-            return 0;
-        }
-        return Math.min(twoCount,fiveCount);
-    }
-
-    public int getFactorCount(int n, int x) {
-        int count = 0;
-        while(n % x == 0) {
-            count++;
-            n /= x;
-        }
-        return count;
+        return res;
     }
 }
