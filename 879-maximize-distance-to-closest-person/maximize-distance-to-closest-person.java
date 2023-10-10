cb8e8f481b9f89df8f89116504c61906e3064808
Jyothi Koushik Paladugu <47804397+jyothi-koushik-1998@users.noreply.github.com>
2023-10-09 23:32:40 -0400

    Time: 2 ms (98.16%) | Memory: 44.9 MB (36.72%) - LeetSync

--- 879-maximize-distance-to-closest-person/maximize-distance-to-closest-person.java
+++ 879-maximize-distance-to-closest-person/maximize-distance-to-closest-person.java
@@ -1,52 +1,20 @@
 class Solution {
     public int maxDistToClosest(int[] seats) {
-        int distance = 0;
-        boolean didStrike1 = false;
-        int[] leftToRight = new int[seats.length];
-        int[] rightToLeft = new int[seats.length];
-
-        for(int i = 0;i < seats.length; i++) {
+        int n = seats.length;
+        int left = -1, right = 0;
+        int max = 0;
+        for(int i=0;i<n;i++) {
             if(seats[i] == 1){
-                didStrike1 = true;
-                distance = 0;
-                leftToRight[i] = 1;
-            } else if(didStrike1){
-                leftToRight[i] = ++distance;
+                left = i;
             } else {
-                leftToRight[i] = 0;
-            }
-        }
-
-        didStrike1 = false;
-        distance = 0;
-        for(int i = seats.length-1;i >= 0; i--) {
-            if(seats[i] == 1){
-                didStrike1 = true;
-                distance = 0;
-                rightToLeft[i] = 1;
-            } else if(didStrike1) {
-                rightToLeft[i] = ++distance;
-            } else {
-                rightToLeft[i] = 0;
-            }
-        }
-
-        int maxDistance = 0;
-        for(int i = 0; i < seats.length; i++) {
-            if(seats[i] != 1) {
-                if((leftToRight[i] == rightToLeft[i])) {
-                    maxDistance = Math.max(maxDistance,leftToRight[i]);
-                } else if(rightToLeft[i] != 0 && leftToRight[i] == 0){
-                    maxDistance = Math.max(maxDistance,rightToLeft[i]);
-                } else if(rightToLeft[i] == 0 && leftToRight[i] != 0){
-                    maxDistance = Math.max(maxDistance,leftToRight[i]);
-                } else {
-                    int min = Math.min(leftToRight[i],rightToLeft[i]);
-                    maxDistance = Math.max(maxDistance,min);
+                while(right < n && (seats[right] == 0 || right < i)){
+                    right++;
                 }
+                int distanceFromLeft = (left == -1) ? n : i-left;
+                int distanceFromRight = (right == n) ? n : right-i;
+                max = Math.max(max,Math.min(distanceFromLeft,distanceFromRight));
             }
         }
-
-        return maxDistance;
+        return max;
     }
 }
