19ef1b58133020c3b4976c598eda6db5b3a358e5
Jyothi Koushik Paladugu <47804397+jyothi-koushik-1998@users.noreply.github.com>
2023-10-14 11:43:43 -0400

    Time: 11 ms (36.68%) | Memory: 48.5 MB (66.20%) - LeetSync

--- 303-range-sum-query-immutable/range-sum-query-immutable.java
+++ 303-range-sum-query-immutable/range-sum-query-immutable.java
@@ -1,44 +1,21 @@
 class NumArray {
-
-    int[] segment;
-    int n;
+    int[] result;
     public NumArray(int[] nums) {
-        this.segment = new int[4 * nums.length];
-        build(0,0,nums.length - 1,nums);
-        this.n = nums.length;
-    }
-    
-    
-    public void build(int index,int left,int right,int[] nums){
-        if(left == right){
-            segment[index] = nums[left];
-            return;
+        result = new int[nums.length];
+        int prefixSum = 0;
+        for(int i = 0; i < result.length; i++) {
+            prefixSum += nums[i];
+            result[i] = prefixSum;
         }
-        
-        int mid = (left + right) >> 1;
-        
-        build(index * 2 + 1,left,mid,nums);
-        build(index * 2 + 2,mid + 1,right,nums);
-        
-        this.segment[index] = segment[index * 2 + 1] + segment[2 * index + 2];
-    }
-    
-    public int query(int left,int right,int ql,int qr,int index){
-        if(qr < left || right < ql){
-            return 0;
-        }
-        
-        if(left >= ql && right <= qr){
-            return segment[index];
-        }
-        
-        int mid = (left + right) >> 1;
-        
-        return query(left,mid,ql,qr,index * 2 + 1) + query(mid + 1,right,ql,qr,index * 2 + 2);
+        System.out.println(Arrays.toString(result));
     }
     
     public int sumRange(int left, int right) {
-        return query(0,n - 1,left,right,0);
+        if(left == 0) {
+            return result[right];
+        } else {
+            return result[right] - result[left-1];
+        }
     }
 }
 
