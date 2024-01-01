7988fd57f856281c75a8af191727e65224171432
Jyothi Koushik Paladugu <47804397+jyothi-koushik-1998@users.noreply.github.com>
2024-01-01 11:43:48 -0500

    Time: 1 ms (26.65%) | Memory: 41.2 MB (26.75%) - LeetSync

--- 289-game-of-life/game-of-life.java
+++ 289-game-of-life/game-of-life.java
@@ -1,6 +1,6 @@
 class Solution {
     public void gameOfLife(int[][] board) {
-         int[][] dir = {
+        int[][] dir = {
                 {-1, -1},
                 {-1, 0},
                 {-1, 1},
@@ -10,12 +10,6 @@ class Solution {
                 {1, 0},
                 {1, 1}
         };
-        StringBuilder[][] track = new StringBuilder[board.length][board[0].length];
-            for(int i = 0; i < board.length; i++) {
-                for(int j = 0; j < board[0].length; j++) {
-                    track[i][j] = new StringBuilder("NN");
-                }
-            }
         for(int i = 0; i < board.length; i++) {
             for(int j = 0; j < board[0].length; j++) {
                 int liveNeighbors = 0;
@@ -23,40 +17,34 @@ class Solution {
                     int nr = i + direction[0];
                     int nc = j + direction[1];
                     if ((nr >= 0 && nr < board.length) && (nc >= 0 && nc < board[0].length)) {
-                        if (track[nr][nc].charAt(0) == 'Y') {
-                            if (track[nr][nc].charAt(1) == 'Y') {
-                                liveNeighbors += (board[nr][nc] == 1) ? 0 : 1;
-                            } else {
-                                liveNeighbors += board[nr][nc];
-                            }
-                        } else {
-                            liveNeighbors += board[nr][nc];
-                        }
+                       if(board[nr][nc] == 2) {
+                           liveNeighbors += 0;
+                       } else if(board[nr][nc] == -1){
+                           liveNeighbors += 1;
+                       } else {
+                           liveNeighbors += board[nr][nc];
+                       }
                     }
-
                 }
-/*
-0 1 0
-0 0 1
-1 1 1
-0 0 0 */
                     if(board[i][j] == 0) {
                         if(liveNeighbors == 3) {
-                            board[i][j] = 1;
-                            track[i][j].setCharAt(1,'Y');
-                        } else {
-                            track[i][j].setCharAt(1,'N');
+                            board[i][j] = 2;
                         }
                     } else {
                         if(liveNeighbors > 3 || liveNeighbors < 2) {
-                            board[i][j] = 0;
-                            track[i][j].setCharAt(1,'Y');
-                        } else {
-                            track[i][j].setCharAt(1,'N');
+                            board[i][j] = -1;
                         }
                     }
-                    track[i][j].setCharAt(0,'Y');
                 }
             }
+        for(int i = 0; i < board.length; i++) {
+            for(int j = 0; j < board[0].length; j++) {
+                if(board[i][j] == 2){
+                    board[i][j] = 1;
+                } else if(board[i][j] == -1) {
+                    board[i][j] = 0;
+                }
+            }
+        }
     }
 }
