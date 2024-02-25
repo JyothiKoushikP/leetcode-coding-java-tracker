db09178bde26768a5f9fa08834154c379a906a67
Jyothi Koushik Paladugu <47804397+JyothiKoushikP@users.noreply.github.com>
2024-02-24 21:45:23 -0500

    Time: 508 ms (8.67%) | Memory: 48.6 MB (5.01%) - LeetSync

---
+++ 1371-minimum-remove-to-make-valid-parentheses/minimum-remove-to-make-valid-parentheses.java
@@ -0,0 +1,31 @@
+class Solution {
+    public String minRemoveToMakeValid(String s) {
+        StringBuilder res = new StringBuilder();
+        Stack<Integer> str = new Stack<Integer>();
+        Stack<Integer> par = new Stack<Integer>();
+        for(int i = 0; i < s.length() ; i++) {
+            char c = s.charAt(i);
+            if(c == '(') {
+                str.push(i);
+                par.push(i);
+            } else if (c == ')') {
+                if(!par.isEmpty()) {
+                    str.push(i);
+                    par.pop();
+                }
+            } else {
+                str.push(i);
+            }
+        }
+        while(!str.isEmpty()) {
+            int index = par.isEmpty() ? -1 : par.peek();
+            if(str.peek() != index) {
+                res.insert(0,s.charAt(str.pop()));
+            } else {
+                par.pop();
+                str.pop();
+            }
+        }
+        return res.toString();
+    }
+}
