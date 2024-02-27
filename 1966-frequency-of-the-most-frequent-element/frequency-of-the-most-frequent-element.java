c52a162baf22ced277c362cdf46eff9facd27481
Jyothi Koushik Paladugu <47804397+JyothiKoushikP@users.noreply.github.com>
2024-02-26 20:58:38 -0500

    Time: 30 ms (63.24%) | Memory: 56.7 MB (58.31%) - LeetSync

--- 1966-frequency-of-the-most-frequent-element/frequency-of-the-most-frequent-element.java
+++ 1966-frequency-of-the-most-frequent-element/frequency-of-the-most-frequent-element.java
@@ -1,38 +1,26 @@
 class Solution {
     public int maxFrequency(int[] nums, int k) {
         Arrays.sort(nums);
-        long[] prefixArray = new long[nums.length];
+        int left = 0;
+        int ans = 0;
+        long curr = 0;
         
-        for(int i = 1; i < nums.length; i++) {
-            prefixArray[i] = nums[i] + prefixArray[i-1];
-        }
-
-        int maxCount = 0;
-        for(int i = 0; i < nums.length; i++) {
-            int count = binarySearch(nums,prefixArray,k,i);
-            maxCount = Math.max(maxCount,count);
-        }
-
-        return maxCount;
-    }
-
-    public int binarySearch(int[] nums, long[] prefix, int k, int i) {
-        int target = nums[i];
-        int start = 0, end = i, best = i;
-        while (start <= end) {
-            int mid = start + (end - start) / 2;
-            long count = (i - mid + 1);
-            long originalSum = prefix[i] - prefix[mid] + nums[mid];
-            long totalSum = (count * target) - originalSum;
-            if(totalSum > k) {
-                start = mid + 1;
-            } else {
-                best = mid;
-                end = mid - 1;
+        for (int right = 0; right < nums.length; right++) {
+            long target = nums[right];
+            curr += target;
+            
+            while ((right - left + 1) * target - curr > k) {
+                curr -= nums[left];
+                left++;
             }
+            
+            ans = Math.max(ans, right - left + 1);
         }
-        return i - best + 1;
+        
+        return ans;
     }
+
+    
 
 
 }
