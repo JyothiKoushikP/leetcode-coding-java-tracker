d3303855d6c32781f8c9d2f5882e31608ea0fcdd
Jyothi Koushik Paladugu <47804397+JyothiKoushikP@users.noreply.github.com>
2024-03-02 11:50:50 -0500

    Time: 4 ms (49.21%) | Memory: 44.3 MB (20.32%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.java
+++ 496-next-greater-element-i/next-greater-element-i.java
@@ -1,26 +1,30 @@
 class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
-        int m = nums1.length, n = nums2.length, pointer = 0;
-        int[] result = new int[m];
-        for(int i: nums1) {
-            result[pointer++] = findGreaterElement(nums2,i);
+        Map<Integer,Integer> indexTrack = new HashMap<>();
+        for(int i = 0; i < nums1.length; i++) {
+            indexTrack.put(nums1[i],i);
         }
-        return result;
-    }
+        Stack<Integer> geStack = new Stack<>();
+        int[] res = new int[nums1.length];
+        for(int i = nums2.length - 1; i >= 0; i--) {
+            while(geStack.size() > 0 && geStack.peek() <= nums2[i]) {
+                geStack.pop();
+            }
 
-    public int findGreaterElement(int[] nums, int target) {
-        int n = nums.length;
-        boolean didFind = false;
-        for(int i = 0; i < n; i++) {
-            if(didFind) {
-                if(nums[i] > target) {
-                    return nums[i];
+            if(indexTrack.containsKey(nums2[i])) {
+                int index = indexTrack.get(nums2[i]);
+                if(geStack.size() == 0) {
+                    res[index] = -1;
+                } else {
+                    res[index] = geStack.peek();
                 }
             }
-            if(nums[i] == target) {
-                didFind = true;
-            }
+
+            geStack.push(nums2[i]);
         }
-        return -1;
+
+        return res;
     }
+
+    
 }
