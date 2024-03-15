94b15f3e80ba5ef1f72bdeff9d4168b3db9d1401
Jyothi Koushik Paladugu <47804397+JyothiKoushikP@users.noreply.github.com>
2024-03-15 15:50:55 -0400

    Time: 17 ms (34.24%) | Memory: 54.5 MB (36.36%) - LeetSync

---
+++ 917-boats-to-save-people/boats-to-save-people.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int numRescueBoats(int[] people, int limit) {
+        Arrays.sort(people);
+        int n = people.length, boats = 0;
+        int start = 0, end = n - 1;
+        while(start <= end) {
+            if(start == end) {
+                boats++;
+                start++;
+                end--;
+                break;
+            }
+            int totalWeight = people[start] + people[end];
+            if(totalWeight > limit) {
+                boats++;
+                end--;
+            } else {
+                boats++;
+                start++;
+                end--;
+            }
+        }
+        return boats;
+    }
+}
