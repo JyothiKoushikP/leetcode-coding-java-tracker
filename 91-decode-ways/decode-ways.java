183415b1df25f7e7cab7ae71db8b62f467042459
Jyothi Koushik Paladugu <47804397+JyothiKoushikP@users.noreply.github.com>
2024-03-17 18:36:28 -0400

    Time: 1 ms (80.90%) | Memory: 40.9 MB (99.76%) - LeetSync

---
+++ 91-decode-ways/decode-ways.java
@@ -0,0 +1,42 @@
+class Solution {
+    public int numDecodings(String s) {
+        if(s.charAt(0) == '0') {
+            return 0;
+        }
+        if(s.length() == 1) {
+            return 1;
+        }
+        int[] dp = new int[s.length() + 1];
+        dp[0] = 1;
+        dp[1] = 1;
+        for(int i = 1; i < s.length() ; i++) {
+            int x = s.charAt(i) - '0';
+            int y = s.charAt(i-1) - '0';
+            if(x == 0) {
+                if(y == 0) {
+                    dp[i+1] = 0;
+                } else {
+                    int res = (y * 10) + x;
+                    if(res >= 1 && res <= 26) {
+                        dp[i+1] = dp[i-1];
+                    } else {
+                        dp[i+1] = 0;
+                    } 
+                }
+            } else {
+                if(y == 0) {
+                    dp[i+1] = dp[i];
+                } else {
+                    int res = (y * 10) + x;
+                    if(res >= 1 && res <= 26) {
+                        dp[i+1] = dp[i] + dp[i-1];
+                    } else {
+                        dp[i+1] = dp[i];
+                    }
+                }    
+            }
+        }
+
+        return dp[dp.length - 1];
+    }
+}
