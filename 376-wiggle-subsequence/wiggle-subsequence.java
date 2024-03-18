9a6b2cd26ca81b764d8c24d20163b04f9d57d52b
Jyothi Koushik Paladugu <47804397+JyothiKoushikP@users.noreply.github.com>
2024-03-17 22:50:35 -0400

    Time: 5 ms (18.63%) | Memory: 41 MB (71.70%) - LeetSync

---
+++ 376-wiggle-subsequence/wiggle-subsequence.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int wiggleMaxLength(int[] nums) {
+       int[] p = new int[nums.length];
+       int[] n = new int[nums.length];
+       int maxlen = 0;
+       p[0] = 0; n[0] = 0;
+       if(nums.length > 1) {
+            if(nums[1] - nums[0] > 0) {
+                p[1]++;
+            } else if(nums[1] - nums[0] < 0) {
+                n[1]++;
+            }
+       }
+       for(int i = 2; i < nums.length; i++) {
+            for(int j = i - 1; j >= 0; j--) {
+                if(nums[i] - nums[j] > 0) {
+                    p[i] = Math.max(p[i],n[j] + 1);
+                } else if(nums[i] - nums[j] < 0) {
+                    n[i] = Math.max(n[i],p[j] + 1);
+                }
+            }
+       }
+
+       for(int i = 0; i < nums.length; i++) {
+            maxlen = Math.max(maxlen,p[i]);
+            maxlen = Math.max(maxlen,n[i]);
+       }
+
+       return maxlen + 1;
+    }
+}
