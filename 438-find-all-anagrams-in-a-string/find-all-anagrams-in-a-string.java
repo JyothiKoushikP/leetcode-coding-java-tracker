4c5f24dc0ca6360dd9a99e6e6264529f55d09286
Jyothi Koushik Paladugu <47804397+JyothiKoushikP@users.noreply.github.com>
2024-03-20 15:17:16 -0400

    Time: 78 ms (24.01%) | Memory: 45.3 MB (29.25%) - LeetSync

--- 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.java
+++ 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.java
@@ -1,36 +1,33 @@
 class Solution {
-    public List<Integer> findAnagrams(String s, String p) {
-        List<Integer> result = new ArrayList();
-        int m = s.length(), n = p.length();
-        int[] freq = new int[26];
-        int[] track = new int[26];
+  public List<Integer> findAnagrams(String s, String p) {
+    List<Integer> res = new ArrayList<>();
+    int k = p.length();
+    Map<Character,Integer> sCount = new HashMap<>();
+    Map<Character,Integer> pCount = new HashMap<>();
+    int left = 0;
 
-        for(int i = 0;i < n; i++) {
-            char ch = p.charAt(i);
-            freq[ch-'a']++;
-        }
-
-        int left = 0, right = 0;
-        while(right < m) {
-            char c = s.charAt(right);
-            track[c-'a']++;
-            if(freq[c-'a'] == 0) {
-                while(left <= right) {
-                    track[s.charAt(left) - 'a']--;;
-                    left++;
-                }
-            } else if(track[c-'a'] > freq[c-'a']){
-                while(left <= right && track[c - 'a'] > freq[c - 'a']) {
-                    track[s.charAt(left) - 'a']--;
-                    left++;
-                }
-            }
-            if((right-left+1) == n) {
-                result.add(left);
-            }
-            right++;
-        }
-
-        return result;
+    for(char ch: p.toCharArray()) {
+        pCount.put(ch,pCount.getOrDefault(ch,0) + 1);
     }
+
+    for(int right = 0; right < s.length(); right++) {
+        char ch = s.charAt(right);
+        sCount.put(ch,sCount.getOrDefault(ch,0) + 1);
+
+        if(right - left + 1 == k) {
+            if(sCount.equals(pCount)){
+                res.add(left);
+            }
+            if(sCount.get(s.charAt(left)) == 1) {
+                sCount.remove(s.charAt(left));
+            } else {
+                sCount.put(s.charAt(left),sCount.get(s.charAt(left)) - 1);
+            }
+            left++;
+        }
+    }
+
+    return res;
+  }
+
 }
