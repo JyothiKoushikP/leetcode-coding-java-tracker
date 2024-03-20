ccafd5cbbbc59de4bc758d3a95da6bc7f5755c82
Jyothi Koushik Paladugu <47804397+JyothiKoushikP@users.noreply.github.com>
2024-03-20 16:11:30 -0400

    Time: 51 ms (25.68%) | Memory: 16.8 MB (43.97%) - LeetSync

--- 1164-missing-number-in-arithmetic-progression/missing-number-in-arithmetic-progression.py
+++ 1164-missing-number-in-arithmetic-progression/missing-number-in-arithmetic-progression.py
@@ -2,9 +2,11 @@ class Solution:
     def missingNumber(self, arr: List[int]) -> int:
         n = len(arr)
         difference = int((arr[n-1] - arr[0]) / n)
-        expected = arr[0]
-        for i in range(n):
-            if not expected == arr[i]:
-                return expected
-            expected += difference
-        return expected
+        start, end = 0, n-1
+        while start < end:
+            mid = int(start + (end - start) / 2);
+            if arr[mid] == (arr[0] + (mid * difference)):
+                start = mid + 1
+            else:
+                end = mid;
+        return arr[0] + (start * difference)
