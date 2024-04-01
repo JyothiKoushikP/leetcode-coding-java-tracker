bf11996edf1db014d74bd36de824b4c5f2e0d125
Jyothi Koushik Paladugu <47804397+JyothiKoushikP@users.noreply.github.com>
2024-04-01 13:19:12 -0400

    Time: 6 ms (59.39%) | Memory: 40.2 MB (76.70%) - LeetSync

--- 650-2-keys-keyboard/2-keys-keyboard.java
+++ 650-2-keys-keyboard/2-keys-keyboard.java
@@ -1,25 +1,38 @@
 class Solution {
     public int minSteps(int n) {
-        return minStepsRecursive(n,"A","",0);
+        // we start with the goal to reach n characters and we start with 1 character
+        return recurse(n, 1);
     }
-
-    public int minStepsRecursive(int n, String org, String copy, int count) {
-        if(org.length() == n) {
-            return count;
+    
+    private int recurse(int n, int len) {
+        // base case: if the goal has been reached no new operations need to be used, so return 0
+        if (len == n)
+            return 0;
+        
+        // since we want the minimum answer among all candidates, 
+        // the result is initially set to the maximum value.
+        int res = Integer.MAX_VALUE;
+        
+        // extra is the len of the copied string, 
+        // nPastes is the number of paste operations performed after the initial copy.
+        int extra = len, nPastes = 0;
+        
+        // repeat while we can successfully paste the text
+        while (len + extra <= n) {
+            // paste the text
+            len += extra;
+            nPastes++;
+            
+            // find answer for the remaining length
+            int remain = recurse(n, len);
+            
+            // if answer is found, update the current result
+            if (remain != -1)
+                res = Math.min(res, nPastes + remain);
         }
-        if(org.length() + copy.length() > n) {
-            return Integer.MAX_VALUE;
-        }
-
-        int minOperations = 0;
-
-        if(copy.isEmpty()) {
-            minOperations =  minStepsRecursive(n,org,copy + org,count + 1);
-        }else if(org.length() == copy.length()) {
-            minOperations =  minStepsRecursive(n,org + copy,copy,count + 1);
-        } else if(org.length() > copy.length()) {
-            minOperations = Math.min(minStepsRecursive(n, org+copy, copy, count + 1), minStepsRecursive(n, org,org , count + 1));
-        }
-        return minOperations;
+        
+        // if answer is not found, return -1,
+        // else return result + 1 (plus one to consider the copy operation we performed initially)
+        return res == Integer.MAX_VALUE ? -1 : res + 1;
     }
 }
