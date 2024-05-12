bc050eb12fce43ac8122e4d209ff386a50b52f78
Jyothi Koushik Paladugu <47804397+JyothiKoushikP@users.noreply.github.com>
2024-05-11 22:07:01 -0400

    Time: 0 ms (100.00%) | Memory: 42 MB (30.76%) - LeetSync

--- 1092-maximum-difference-between-node-and-ancestor/maximum-difference-between-node-and-ancestor.java
+++ 1092-maximum-difference-between-node-and-ancestor/maximum-difference-between-node-and-ancestor.java
@@ -15,32 +15,42 @@
  */
 class Solution {
     int[] mdf = new int[1];
-    List<Integer> res = new ArrayList();
+    int[] maxdf = new int[2];
+
 
     public int maxAncestorDiff(TreeNode root) {
         mdf[0] = Integer.MIN_VALUE;
+        maxdf[0] = root.val;
+        maxdf[1] = root.val;
         getMaxDiff(root);
         return mdf[0];
     }
 
     public void getMaxDiff(TreeNode node) {
         if(node.left == null && node.right == null) {
-            for(int i = 0; i < res.size(); i++) {
-                mdf[0] = Math.max(mdf[0],Math.abs(res.get(i) - node.val));
-            }
+            mdf[0] = Math.max(mdf[0],Math.abs(node.val - maxdf[0]));
+            mdf[0] = Math.max(mdf[0],Math.abs(node.val - maxdf[1]));
             return;
         }
-        for(int i = 0; i < res.size(); i++) {
-            mdf[0] = Math.max(mdf[0],Math.abs(res.get(i) - node.val));
-        }
-        res.add(node.val);
+        mdf[0] = Math.max(mdf[0],Math.abs(node.val - maxdf[0]));
+        mdf[0] = Math.max(mdf[0],Math.abs(node.val - maxdf[1]));
+
+        int previousMin = maxdf[0];
+        int previousMax = maxdf[1];
+
+        maxdf[0] = Math.min(maxdf[0],node.val);
+        maxdf[1] = Math.max(maxdf[1],node.val);
+
         if(node.left != null) {
             getMaxDiff(node.left);
         }
         if(node.right != null) {
             getMaxDiff(node.right);
         }
-        res.remove(res.size() - 1);
+        
+        maxdf[0] = previousMin;
+        maxdf[1] = previousMax;
+
     }
 
 }
