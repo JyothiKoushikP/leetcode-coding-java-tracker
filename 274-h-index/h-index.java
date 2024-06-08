1a19b08f85ea78ceb84523910589e8ad32d239b3
Jyothi Koushik Paladugu <47804397+JyothiKoushikP@users.noreply.github.com>
2024-06-08 19:30:21 -0400

    Time: 0 ms (100.00%) | Memory: 41.3 MB (57.30%) - LeetSync

--- 274-h-index/h-index.java
+++ 274-h-index/h-index.java
@@ -1,18 +1,33 @@
 class Solution {
+    // public int hIndex(int[] citations) {
+    //     Arrays.sort(citations);
+    //     Map<Integer,Integer> record = new HashMap<>();
+    //     int minValue = Integer.MAX_VALUE;
+    //     int last = citations.length - 1;
+    //     for(int i = citations.length - 1; i >= 0; i--) {
+    //         minValue = Math.min(minValue, citations[i]);
+    //         record.put(minValue, (last - i) + 1);
+    //     }
+    //     int h_index = Integer.MIN_VALUE;
+    //     for(int i: record.keySet()) {
+    //         int c = i, p = record.get(c);
+    //         h_index = Math.max(h_index, Math.min(c,p));
+    //     }
+    //     return h_index;
+    // }
+
     public int hIndex(int[] citations) {
-        Arrays.sort(citations);
-        Map<Integer,Integer> record = new HashMap<>();
-        int minValue = Integer.MAX_VALUE;
-        int last = citations.length - 1;
-        for(int i = citations.length - 1; i >= 0; i--) {
-            minValue = Math.min(minValue, citations[i]);
-            record.put(minValue, (last - i) + 1);
+        int n = citations.length;
+        int[] papers = new int[citations.length + 1];
+        for(int i: citations) {
+            papers[Math.min(i,n)] += 1;
         }
-        int h_index = Integer.MIN_VALUE;
-        for(int i: record.keySet()) {
-            int c = i, p = record.get(c);
-            h_index = Math.max(h_index, Math.min(c,p));
+        int sum = 0;
+        int maxValue = Integer.MIN_VALUE;
+        for(int i = papers.length - 1; i >= 0; i--) {
+            sum += papers[i];
+            maxValue = Math.max(maxValue, Math.min(i,sum));
         }
-        return h_index;
+        return maxValue;
     }
 }
