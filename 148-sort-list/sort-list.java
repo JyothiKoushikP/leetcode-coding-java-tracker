bd61fe1be00cb6bea1a7be57361f82a23b842a6e
Jyothi Koushik Paladugu <47804397+JyothiKoushikP@users.noreply.github.com>
2024-06-08 20:09:27 -0400

    Time: 12 ms (32.41%) | Memory: 56.9 MB (42.47%) - LeetSync

--- 148-sort-list/sort-list.java
+++ 148-sort-list/sort-list.java
@@ -10,43 +10,43 @@
  */
 class Solution {
     public ListNode sortList(ListNode head) {
-        if (head == null || head.next == null) {
+        if(head == null || head.next == null) {
             return head;
         }
-
         ListNode mid = getMid(head);
         ListNode left = sortList(head);
         ListNode right = sortList(mid);
 
-        return mergeLists(left, right);
+        return mergeList(left,right);
     }
 
-    public ListNode mergeLists(ListNode list1, ListNode list2) {
-        ListNode dummyHead = new ListNode();
-        ListNode tail = dummyHead;
-        while (list1 != null && list2 != null) {
-            if (list1.val < list2.val) {
-                tail.next = list1;
-                list1 = list1.next;
-                tail = tail.next;
+    public ListNode mergeList(ListNode l, ListNode r) {
+        ListNode head = new ListNode(-1);
+        ListNode dummy = head;
+        while(l != null && r != null) {
+            if(l.val <= r.val) {
+                dummy.next = l;
+                l = l.next;
             } else {
-                tail.next = list2;
-                list2 = list2.next;
-                tail = tail.next;
+                 dummy.next = r;
+                 r = r.next;
             }
+            dummy = dummy.next;
         }
-        tail.next = (list1 != null) ? list1 : list2;
-        return dummyHead.next;
+        dummy.next = (l != null) ? l : r;
+        return head.next;
     }
 
-    public ListNode getMid(ListNode head) {
-        ListNode midPrev = null;
-        while (head != null && head.next != null) {
-            midPrev = (midPrev == null) ? head : midPrev.next;
-            head = head.next.next;
+    public ListNode getMid(ListNode lh) {
+        ListNode slow = lh, fast = lh;
+        ListNode prev = null;
+        while(fast != null && fast.next != null) {
+            prev = slow;
+            slow = slow.next;
+            fast = fast.next.next;
         }
-        ListNode mid = midPrev.next;
-        midPrev.next = null;
-        return mid;
+        prev.next = null;
+        return slow;
     }
+
 }
