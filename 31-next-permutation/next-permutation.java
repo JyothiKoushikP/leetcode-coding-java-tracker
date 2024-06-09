5e2d1efee21d64b7d065590bfc03b69628290783
Jyothi Koushik Paladugu <47804397+JyothiKoushikP@users.noreply.github.com>
2024-06-08 20:45:30 -0400

    Time: 0 ms (100.00%) | Memory: 42.4 MB (98.63%) - LeetSync

--- 31-next-permutation/next-permutation.java
+++ 31-next-permutation/next-permutation.java
@@ -1,3 +1,16 @@
+/*
+TC: O(n)
+SC: O(1)
+Approach:
+from the right, find the index e such that a[e] > a[e-1]
+now again scan from the end of the array and get the first element, which is greater than
+a[e-1]
+swap both (x,e-1)
+if e == 0, swap the whole array
+then from e to the end of the array, swap the subarray, that is your answer
+*/
+
+
 class Solution {
     public void nextPermutation(int[] a) {
         int p = a.length - 1;
@@ -26,10 +39,9 @@ class Solution {
         } 
     }
 
-
-    public void swap(int[] a, int x, int y) {
-        int temp = a[x];
-        a[x] = a[y];
-        a[y] = temp;
+    public void swap(int[] arr, int x, int y) {
+        int temp = arr[x];
+        arr[x] = arr[y];
+        arr[y] = temp;
     }
 }
