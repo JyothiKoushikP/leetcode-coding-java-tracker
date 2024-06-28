6b0e67c4059c6f0ac3085899585d8343659d1c0e
Jyothi Koushik Paladugu <47804397+JyothiKoushikP@users.noreply.github.com>
2024-06-28 14:27:33 -0400

    Time: 163 ms (56.67%) | Memory: 17.6 MB (90.00%) - LeetSync

--- 1190-smallest-common-region/smallest-common-region.py
+++ 1190-smallest-common-region/smallest-common-region.py
@@ -9,32 +9,27 @@ class Solution(object):
             for child in children:
                 hierarchy_map[child] = parent
 
-        # Check if region1 or region2 is not in the hierarchy map
         if region1 not in hierarchy_map or region2 not in hierarchy_map:
             return None
 
-        # If region2 is a direct child of region1 or region1 has no parent
         if hierarchy_map[region2] == region1 or hierarchy_map[region1] is None:
             return region1
-        # If region1 is a direct child of region2 or region2 has no parent
         if hierarchy_map[region1] == region2 or hierarchy_map[region2] is None:
             return region2
 
-        # Collect all ancestors of region1
         upper_states_r1 = set()
         current_region1 = region1
         while current_region1 is not None:
             upper_states_r1.add(current_region1)
             current_region1 = hierarchy_map[current_region1]
 
-        # Collect all ancestors of region2 and find common ancestor
         current_region2 = region2
         while current_region2 is not None:
             if current_region2 in upper_states_r1:
                 return current_region2
             current_region2 = hierarchy_map[current_region2]
 
-        return None  # If no common ancestor found
+        return None  
 
         
         
