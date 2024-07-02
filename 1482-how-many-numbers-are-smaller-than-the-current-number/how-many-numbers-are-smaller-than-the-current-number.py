7cd4355325e559522eeb730b4d19a9f268acb082
Jyothi Koushik Paladugu <47804397+JyothiKoushikP@users.noreply.github.com>
2024-07-02 15:35:49 -0400

    Time: 22 ms (99.07%) | Memory: 11.7 MB (56.72%) - LeetSync

--- 1482-how-many-numbers-are-smaller-than-the-current-number/how-many-numbers-are-smaller-than-the-current-number.py
+++ 1482-how-many-numbers-are-smaller-than-the-current-number/how-many-numbers-are-smaller-than-the-current-number.py
@@ -1,14 +1,21 @@
-class Solution:
-    def smallerNumbersThanCurrent(self, nums: List[int]) -> List[int]:
-        ps = [0] * 102
+class Solution(object):
+    def smallerNumbersThanCurrent(self, nums):
+        maxi = max(nums)
+        freq = [0] * (maxi + 1)
+
         for i in nums:
-            ps[i] += 1
-
-        for i in range(1, len(ps)):
-            ps[i] += ps[i - 1]
-
-        for i in range(len(nums)):
-            if nums[i] != 0:
-                nums[i] = ps[nums[i] - 1]
+            freq[i] += 1
         
-        return nums
+        for i in range(1,len(freq)):
+            freq[i] += freq[i-1]
+        
+        result = [0] * len(nums)
+        for i in range(len(nums)):
+            x = nums[i]
+            result[i] = freq[x-1] if x >= 1 else 0
+
+        return result
+
+
+
+        
