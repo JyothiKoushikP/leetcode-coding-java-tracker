18966bb0d369bf0192bb1afa4e922bc13bf374c2
Jyothi Koushik Paladugu <47804397+JyothiKoushikP@users.noreply.github.com>
2024-07-03 16:19:53 -0400

    Time: 1 ms (52.16%) | Memory: 42.4 MB (84.48%) - LeetSync

--- 78-subsets/subsets.java
+++ 78-subsets/subsets.java
@@ -1,15 +1,24 @@
 class Solution {
     public List<List<Integer>> subsets(int[] nums) {
-        List<List<Integer>> result = new ArrayList();
+        List<List<Integer>> result = new ArrayList<>();
+        List<Integer> curr = new ArrayList<>();
+        generateParanthesis(nums, curr, 0, result);
         result.add(new ArrayList());
-        for(int i = 0; i < nums.length; i++) {
-            int n = result.size();
-            for(int j = 0; j < n; j++) {
-                List<Integer> innerArray = new ArrayList<>(result.get(j));
-                innerArray.add(nums[i]);
-                result.add(innerArray);
-            }
-        }
         return result;
+    }
+
+    public void generateParanthesis(int[] nums, List<Integer> arr, int pointer, List<List<Integer>> result) {
+        if(pointer == nums.length) {
+            return;
+        }
+        for(int i = pointer; i < nums.length; i++) {
+            arr.add(nums[i]);
+            List<Integer> l1 = new ArrayList<>(arr);
+            result.add(l1);
+            generateParanthesis(nums, arr, i + 1, result);
+            arr.remove(arr.size() - 1);
+        }
+
+        return;
     }
 }
