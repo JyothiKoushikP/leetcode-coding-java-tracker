198e1f1a594dc3bd334216444c4af8c9c7e841b4
Jyothi Koushik Paladugu <47804397+JyothiKoushikP@users.noreply.github.com>
2024-07-07 00:57:01 -0400

    Time: 51 ms (92.31%) | Memory: 17.7 MB (27.01%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -1,10 +1,8 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        track = {}
+        d = {}
         for i in range(len(nums)):
-            nTwo = target - nums[i]
-            if nTwo in track and track[nTwo] != i:
-                return [track[nTwo], i]
-            track[nums[i]] = i
-
-        return [-1, -1]
+            if d.get(target-nums[i])!=None:
+                return [d.get(target-nums[i]),i]
+            d[nums[i]] = i
+        return [-1,-1]
